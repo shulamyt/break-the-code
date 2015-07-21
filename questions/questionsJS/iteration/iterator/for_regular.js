1625ee017fe79bd33140202e6899b642fd1eb555
Shulamyt Ajami <shulamyt@gmail.com>
2015-07-21 14:23:42 +0300

    ues more generic code

--- questions/questionsJS/iteration/iterator/for_regular.js
+++ questions/questionsJS/iteration/iterator/for_regular.js
@@ -1,12 +1,8 @@
-var array = ['a', 'b', 'c'];
-//generic!!
-var c = new Continer(['a', 'b', 'c']);
-c.iter();
-var iterator = new Itrerator(array);//array.iterator();
+var continer = new Continer(['a', 'b', 'c']);
+var iterator = continer.iterator();
 print("1");
 while(iterator.hasNext()){
     print("2");
     print(iterator.next().value);
-    //print(iterator.next()); -- pointer???
 }
 print("3");
