2d4b12727ee10f319936e5256e915c173b960001
Shulamyt Ajami <shulamyt@gmail.com>
2015-10-21 16:08:19 +0300

    seperate between local host and server start script

--- start_server_localhost.js
+++ start_server_localhost.js
@@ -6,9 +6,9 @@ app.use(express.static('target'));
 app.use(bodyParser.json());
 
 //for local host:
-//var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
 //for server:
-var server = app.listen(8080, 'getTheCodes', function () {
+// var server = app.listen(8080, 'getTheCodes', function () {
 
   var host = server.address().address;
   var port = server.address().port;
