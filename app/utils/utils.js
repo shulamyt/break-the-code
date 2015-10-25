91f882b32278d3a9e6c019a3cf55b25ddaa88c0f
Shulamyt <shulamyt@gmail.com>
2015-10-25 17:28:25 +0200

    css office

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -21,4 +21,19 @@ define([
       return input;
     };
   });
+
+  app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+  });
+  
 });
