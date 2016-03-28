8b30c352e6ab3b1f6d59626a0fb1383c84fde4c4
Shulamyt Ajami <shulamyt@gmail.com>
2016-03-28 15:55:24 +0300

    support nextQuestionId property in configuration
    nextQuestionId

--- server/services/testPlanService.js
+++ server/services/testPlanService.js
@@ -34,9 +34,61 @@ TestPlanService.prototype.getTestPlan = function() {
 		var questionsInfo = configuration.questionsInfo;
 		var chance = new Chance();
 		var testPlan = chance.pick(questionsInfo, numOfQuestionsForPlan);
+		console.log("before change: " + testPlan.map(function(question){
+			return question.id;
+		}));
+		self.addExtraQuestions(testPlan, questionsInfo);
+		console.log("after change: " + testPlan.map(function(question){
+			return question.id;
+		}));
 		resolve(testPlan);
 	});
 	return promise;
+};
+
+TestPlanService.prototype.getQuestionsInfoMap = function(questionsInfo) {
+	var questionsInfoMap = new Map();
+	questionsInfo.forEach(function(question){
+		questionsInfoMap.set(question.id, question);
+	});
+	return questionsInfoMap;
+};
+
+TestPlanService.prototype.addExtraQuestions = function(testPlan, questionsInfo) {
+	var index = 0;
+	var questionsInfoMap = this.getQuestionsInfoMap(questionsInfo);
+	while (index < testPlan.length){
+		index = this.addQuestion(index, testPlan, questionsInfoMap);
+		index = index + 1;
+	}
+	return testPlan;
+};
+
+TestPlanService.prototype.removeQuestion = function(questionId, testPlan) {
+	for(var i in testPlan){
+		var question = testPlan[i];
+		if(question.id === questionId){
+			testPlan.splice(i, 1);
+			return i;
+		}
+	}
+};
+
+TestPlanService.prototype.addQuestion = function(index, testPlan, questionsInfoMap){
+	var currentQuestion = testPlan[index];
+	var nextQuestionId = currentQuestion.nextQuestionId;
+	if(typeof(nextQuestionId)!=='undefined' && nextQuestionId !== null){
+		var removedIndex = this.removeQuestion(nextQuestionId, testPlan);
+		if(typeof(removedIndex) !== 'undefined' && removedIndex < index){
+			index = index - 1;
+		}
+		var nextQuestion = questionsInfoMap.get(nextQuestionId);
+		index = index + 1;
+		testPlan.splice(index, 0, nextQuestion);
+		this.addQuestion(index, testPlan, questionsInfoMap);
+		return index;
+	}
+	return index;
 };
 
 var testPlanService = new TestPlanService();
