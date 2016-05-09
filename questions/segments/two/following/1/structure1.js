37dea342b28c601d5efcb2e76e0127c7d3bdd316
Shulamyt Ajami <shulamyt@gmail.com>
2016-05-09 21:40:30 +0300

    q

---
+++ questions/segments/two/following/1/structure1.js
@@ -0,0 +1,20 @@
+var x = 33;
+if ( x>10 ) {
+	if ( x<40 ) {
+		if ( x<20 ) {
+			print ("IN");	
+		}
+		else if ( x>30 ) {
+			print ("IN");
+		}
+		else {
+			print ("OUT");
+		}
+	}
+	else {
+		print ("OUT");
+	}
+}
+else {
+	print ("OUT");
+}
