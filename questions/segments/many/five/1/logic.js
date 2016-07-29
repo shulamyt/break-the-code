c516c239dc4208b07535dcede47ad54a55b5f959
Shulamyt Ajami <shulamyt@gmail.com>
2016-07-29 12:32:09 +0300

    remove old q

--- questions/segments/many/five/1/logic.js
+++
@@ -1,6 +0,0 @@
-var x = 33;
-if ( x>0 && x<10 || x>20 && x<30 || x>30 && x<40 || x>40 && x<50 ) {
-	print ("IN");
-} else {
-	print ("OUT");
-}
--- questions/segments/many/five/1/logic2.js
+++
@@ -1,6 +0,0 @@
-var x = 33;
-if ( x>0 && x<50 && !( x>10 && x<20 ) && !( x>30 && x<40 ) ) {
-	print ("IN");
-} else {
-	print ("OUT");
-}
