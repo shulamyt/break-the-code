6072dffdc2d74f7f7dbd084681d9b82b1b458765
Shulamyt Ajami <shulamyt@gmail.com>
2016-07-31 21:23:02 +0300

    fix forArray q

--- questions/segments/many/four/forArray.js
+++ questions/segments/many/four/forArray.js
@@ -2,7 +2,7 @@ var x = 51;
 var a = [[0,10] , [20,30] , [40,50] , [60,70]];
 var tmp = "1";
 for (var i=0 ; i<a.length ; i++) {
-	if (x<a[i][0] && x>a[i][1]) {
+	if (x>a[i][0] && x<a[i][1]) {
 		tmp = "2";
 		break;
 	}
--- questions/segments/many/two/forArray.js
+++ questions/segments/many/two/forArray.js
@@ -2,7 +2,7 @@ var x = 44;
 var a = [[0,10] , [20,30]];
 var tmp = "1";
 for (var i=0 ; i<a.length ; i++) {
-	if (x<a[i][0] && x>a[i][1]) {
+	if (x>a[i][0] && x<a[i][1]) {
 		tmp = "2";
 		break;
 	}
