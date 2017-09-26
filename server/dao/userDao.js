ce11721c3af094259ef8323426d780506f73131a
shulamyt <shulamyt@gmail.com>
2017-09-26 12:44:51 +0300

    fix syntax

--- server/dao/userDao.js
+++ server/dao/userDao.js
@@ -5,14 +5,12 @@ function UserDao(){};
 
 UserDao.prototype.update = function(user){
     var promise = new Promise(function(resolve, reject) {
-        client.connect(function(){
-            var queryStr = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
-            client.query(queryStr, (err, res) => {
-                if (err) throw err;
-                client.end();
-                resolve(res);
-            })
-        }).catch(err => console.error('connection error', err.stack));
+        client.connect();
+        var queryStr = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
+        client.query(queryStr)
+            .then(result => resolve(result))
+            .catch(e => console.error(e.stack))
+            .then(() => client.end());
     });
     return promise;
 };
@@ -20,14 +18,12 @@ UserDao.prototype.update = function(user){
 UserDao.prototype.save = function(user){
     //console.log("userDao is going to save user...");
     var promise = new Promise(function(resolve, reject) {
-        client.connect(function(){
-            var queryStr = dbUtils.jsonToInsertQuery(user, "Experimenter", ["questions", "testPlan", "groupNum"]);
-            client.query(queryStr, (err, res) => {
-                if (err) throw err;
-                client.end();
-                resolve(res);
-            })
-        }).catch(err => console.error('connection error', err.stack));
+        client.connect();
+        var queryStr = dbUtils.jsonToInsertQuery(user, "Experimenter", ["questions", "testPlan", "groupNum"]);
+        client.query(queryStr)
+            .then(result => resolve(result))
+            .catch(e => console.error(e.stack))
+            .then(() => client.end());
     });
     return promise;
 };
