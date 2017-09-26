10c7498393872c83e60b6ff66a10fcc6ed0e35b6
shulamyt <shulamyt@gmail.com>
2017-09-26 12:28:29 +0300

    fupdate syntax

--- server/dao/userDao.js
+++ server/dao/userDao.js
@@ -5,20 +5,14 @@ function UserDao(){};
 
 UserDao.prototype.update = function(user){
     var promise = new Promise(function(resolve, reject) {
-        client.connect(function(err, client, done) {
-            if(err) {
-                return console.error('error fetching client from pool', err);
-            }
-            var query = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
-            client.query(query, function(err, result) {
-                done();
-                if(err) {
-                    return console.error('error running query', err);
-                }else{
-                    resolve(result);
-                }
+        client.connect(function(){
+            var queryStr = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
+            client.query(queryStr, (err, res) => {
+                if (err) throw err;
+                client.end();
+                resolve(res);
             })
-        });
+        }).catch(err => console.error('connection error', err.stack));
     });
     return promise;
 };
@@ -26,24 +20,14 @@ UserDao.prototype.update = function(user){
 UserDao.prototype.save = function(user){
     //console.log("userDao is going to save user...");
     var promise = new Promise(function(resolve, reject) {
-        //console.log("userDao is asking for connection...");
-        client.connect(function(err, client, done) {
-            if(err) {
-                //console.log("userDao - no connection for you!");
-                return console.error('error fetching client from pool', err);
-            }
-            //console.log("userDao has no error when ask for connection!!!!! :)");
-            var query = dbUtils.jsonToInsertQuery(user, "Experimenter", ["questions", "testPlan", "groupNum"]);
-            client.query(query, function(err, result) {
-                done();
-                if(err) {
-                    return console.error('error running query', err);
-                }else{
-                    //console.log("userDao secssessss!!!!! :)");
-                    resolve(result);
-                }
+        client.connect(function(){
+            var queryStr = dbUtils.jsonToInsertQuery(user, "Experimenter", ["questions", "testPlan", "groupNum"]);
+            client.query(queryStr, (err, res) => {
+                if (err) throw err;
+                client.end();
+                resolve(res);
             })
-        });
+        }).catch(err => console.error('connection error', err.stack));
     });
     return promise;
 };
