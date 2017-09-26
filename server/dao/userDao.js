b15af657ef0006ca13f4e0fbeea341b37b84d355
shulamyt <shulamyt@gmail.com>
2017-09-26 13:39:41 +0300

    try connction

--- server/dao/userDao.js
+++ server/dao/userDao.js
@@ -5,27 +5,48 @@ function UserDao(){};
 
 UserDao.prototype.update = function(user){
     var promise = new Promise(function(resolve, reject) {
-        client.connect();
+        // client.connect();
         var queryStr = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
-        client.query(queryStr)
-            .then(result => resolve(result))
-            .catch(e => console.error(e.stack))
-            .then(() => client.end());
+        client.connect(err => {
+            if (err) {
+                console.error(err.stack);
+                return;
+            }
+            client.query(queryStr, [], (err, data) => {
+                if (err) {
+                    console.error(err.stack);
+                    client.end();
+                } else {
+                    client.end();
+                    resolve(data);
+                }
+            });
+        });
+        //client.query(queryStr)
+            // .then(result => resolve(result))
+            // .catch(e => console.error(e.stack))
+            // .then(() => client.end());
     });
     return promise;
 };
 
 UserDao.prototype.save = function(user){
-    //console.log("userDao is going to save user...");
-    var promise = new Promise(function(resolve, reject) {
-        client.connect();
-        var queryStr = dbUtils.jsonToInsertQuery(user, "Experimenter", ["questions", "testPlan", "groupNum"]);
-        client.query(queryStr)
-            .then(result => resolve(result))
-            .catch(e => console.error(e.stack))
-            .then(() => client.end());
+    var queryStr = dbUtils.jsonToUpdateQuery(user, "Experimenter", ["questions", "testPlan"]);
+    client.connect(err => {
+        if (err) {
+            console.error(err.stack);
+            return;
+        }
+        client.query(queryStr, [], (err, data) => {
+            if (err) {
+                console.error(err.stack);
+                client.end();
+            } else {
+                client.end();
+                resolve(data);
+            }
+        });
     });
-    return promise;
 };
 
 var userDao = new UserDao();
