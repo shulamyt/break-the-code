3e4cad2748d04a86b702770969fa5ede0057c098
shulamyt <shulamyt@gmail.com>
2017-12-02 21:57:53 +0200

    ethics approval missing lines

--- react/src/welcome-page.jsx
+++ react/src/welcome-page.jsx
@@ -14,10 +14,11 @@ class WelcomePage extends Component {
         <div className="header"></div>
         <div className="explain">
           <p>This site is part of an experiment in software engineering which aims to measure the comprehension of different code snippets. Note that the goal is to characterize the different code snippets, not to evaluate the programmers!</p>
-          <p>During the game you will be given code snippets of varying difficulty drawn randomly from a pool. There is a time limit  for each question. Try to answer every question as quickly as possible (and of course correctly, but please think it out and do not guess!). Remember that your score depends on how fast and how correctly you answered.</p>
+          <p>During the game you will be given code snippets of varying difficulty drawn randomly from a pool. There is a time limit  for each question. Try to answer every question as quickly as possible (and of course correctly, but please think it out and do not guess!). Remember that your score depends on how fast and how correctly you answered. The total time is expected to be about 15 minutes.</p>
           <p>It is important to note:</p>
-          <p>We do not collect any identifying information. All information gathered is anonymous for research purposes only.</p>
+          <p>We do not collect any identifying information (not even IP addresses). All information gathered is anonymous for research purposes only.</p>
           <p>You may retire at any point (even though we'd be happy if you stay with us until the end).  For any concerns contact: shulamyt@gmail.com</p>
+          <p>Continuing to the questionnaire indicates agreement to participate in the experiment.</p>
           <p>Good luck!</p>
         </div>
         <div className="hacker"></div>
